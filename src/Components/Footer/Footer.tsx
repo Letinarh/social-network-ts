cb729518c5c99c44b9634852083aaf2c0eaa50d8
Vladimir Bukhta <bukhtavladimir@gmail.com>
2022-04-17 22:42:53 +0300

    Update Footer.tsx
    foter

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,10 +3,11 @@ import s from './Footer.module.css';
 
 const Footer = () =>{
     return <footer className={s.footer}>
-        <p>Prosto footer</p>
+        <p>Simple footer</p>
         <div className={s.fContent}>
 
             <img src="https://icon-library.com/images/waiting-icon-gif/waiting-icon-gif-29.jpg" width="70" height="70" alt=""/>
+            Are you serious read that?
         </div>
 
     </footer>
