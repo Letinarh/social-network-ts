14ae91622dbe3bb550d722cfb42ba58dba758f6c
Vladimir Bukhta <bukhtavladimir@gmail.com>
2022-07-07 22:57:32 +0300

    work at users page
    Create reducers
    follow, unFollow, setUsers, showMore

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -1,6 +1,7 @@
 import {combineReducers, createStore} from "redux";
 import {dialogsReducer} from "./dialogsReducer";
 import {profileReducer} from "./profile-reducer";
+import usersReducer from "./users-reducer";
 
 
 export type RootStateType = ReturnType<typeof rootReducer>
@@ -9,6 +10,8 @@ export type RootStateType = ReturnType<typeof rootReducer>
 let rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
+ // sidebar: sidebarReducer,
+    usersPage: usersReducer,
 })
 
 export type  AppStateType = ReturnType<typeof rootReducer>
--- src/Redux/users-reducer.ts
+++ src/Redux/users-reducer.ts
@@ -2,9 +2,10 @@ export enum actionCreatorTypes {
     FOLLOW = "FOLLOW-USER",
     UNFOLLOW = "UNFOLLOW-USER",
     SHOW_MORE= "SHOW-MORE",
+    SET_USERS= "SET-USERS",
 
 }
-type user = {
+export type userType = {
     id:string
     fullName:string
     range: "Support" | "Hang Around"| "Nomad "|"Prospect"| "Member "
@@ -13,62 +14,82 @@ type user = {
         country: string
     }
     followed: boolean
-
-
 }
-type usersPageStoreType = {
-    users: Array<user>,
+export type usersPageStoreType = {
+    users: Array<userType>,
 }
+export type actionsType = followACType | unFollowACType | showMoreACType | setUsersACType
+
 
 let initialState:usersPageStoreType = {
     users: [
-        {
+       /* {
             id: "0",
-            followed: false,
             fullName: "Vladimir Bukhta  ",
             range: "Support",
-            location: {city: "Drogichin", country: "Belarus"}
+            location: {city: "Drogichin", country: "Belarus"},
+            followed: false,
         },
         {
             id: "1",
-            followed: true,
             fullName: "Alexandr Smirnov  ",
             range: "Hang Around",
-            location: {city: "Kyiv", country: "Ukraine"}
+            location: {city: "Kyiv", country: "Ukraine"},
+            followed: true,
         },
         {
             id: "2",
-            followed: true,
             fullName: "Victor Kosyak  ",
             range: "Hang Around",
-            location: {city: "Brest", country: "Belarus"}
+            location: {city: "Brest", country: "Belarus"},
+            followed: true,
         },
         {
             id: "3",
-            followed: false,
             fullName: "Ivan Kozak  ",
             range: "Support",
-            location: {city: "Brest", country: "Belarus"}
-        },
+            location: {city: "Brest", country: "Belarus"},
+            followed: false,
+        },*/
     ],
 }
 
-type followACType = { type: actionCreatorTypes.FOLLOW, payload: string}
-type unfollowACType = { type: actionCreatorTypes.UNFOLLOW, }
+type followACType = { type: actionCreatorTypes.FOLLOW, userId: string}
+type unFollowACType = { type: actionCreatorTypes.UNFOLLOW, userId: string}
 type showMoreACType = { type: actionCreatorTypes.SHOW_MORE, }
+type setUsersACType = { type: actionCreatorTypes.SET_USERS, users: Array<userType>}
 
 
- const followAC  = () =>  ({ type: actionCreatorTypes.FOLLOW, payload: this.})
- const unfollowAC = () => ({ type: actionCreatorTypes.UNFOLLOW })
- const showMoreAC = () => ({ type: actionCreatorTypes.UNFOLLOW })
-type actionsType = followACType | unfollowACType | showMoreACType
+
+export const followAC  = (userId:string) =>  ({ type: actionCreatorTypes.FOLLOW, userId})
+export const unFollowAC = (userId:string) => ({ type: actionCreatorTypes.UNFOLLOW, userId})
+export const showMoreAC = () => ({ type: actionCreatorTypes.UNFOLLOW })
+export const setUsersAC = (users:Array<userType>) => ({type:actionCreatorTypes.SET_USERS, users})
+
+
 
 export const usersReducer = (state: usersPageStoreType = initialState, action: actionsType): usersPageStoreType => {
     switch (action.type) {
         case actionCreatorTypes.FOLLOW:
-
+            return  {
+                ...state,
+                users: state.users.map(u =>{
+                    if (u.id === action.userId)
+                        return {...u, followed: true}
+                    return u
+                })
+            }
         case actionCreatorTypes.UNFOLLOW:
-
+            return  {
+                ...state,
+                users: state.users.map(u =>{
+                    if (u.id === action.userId)
+                        return {...u, followed: false}
+                    return u
+                })
+            }
+        case actionCreatorTypes.SET_USERS:
+            return {...state, users:[...state.users, ...action.users]}
 
         default:
             return {...state}
