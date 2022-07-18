d63ec59f1e06d1de577286ce592bd756ea519b22
Vladimir <bukhtavladimir@gmail.com>
2022-07-18 14:59:38 +0300

    53 start

--- src/Components/Users/Users.tsx
+++ src/Components/Users/Users.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {RootStateType} from "../../Redux/redux-store";
 import {followAC, setUsersAC, unFollowAC, userType} from "../../Redux/users-reducer";
@@ -7,9 +7,8 @@ import axios from "axios";
 import userAvaDefault from "../../Assets/images/Ninja-Avatar.jpg"
 
 
-
 const Users = () => {
-    const usersData = useSelector<RootStateType,Array<userType>>(state => state.usersPage.users)
+    const usersData = useSelector<RootStateType, Array<userType>>(state => state.usersPage.users)
     const dispatch = useDispatch();
 
 
@@ -17,7 +16,7 @@ const Users = () => {
         {
             id: "0",
             name: "Vladimir Bukhta  ",
-            photos:{
+            photos: {
                 large: "https://freepngimg.com/save/73526-bearded-skull-illustration-vector-drawing-beard/1000x1000",
                 small: ""
             },
@@ -29,7 +28,7 @@ const Users = () => {
         {
             id: "1",
             name: "Alexandr Smirnov  ",
-            photos:{
+            photos: {
                 large: "https://us.123rf.com/450wm/jemastock/jemastock2104/jemastock210400572/167646451-motorcycle-handlebars-retro-style-icon.jpg?ver=6",
                 small: ""
             },
@@ -41,7 +40,7 @@ const Users = () => {
         {
             id: "2",
             name: "Victor Kosyak  ",
-            photos:{
+            photos: {
                 small: "",
                 large: "https://i.pinimg.com/474x/82/6e/5c/826e5c628591719deba54c9b9ba10324--smiley-vikings.jpg",
             },
@@ -53,9 +52,9 @@ const Users = () => {
         {
             id: "3",
             name: "Ivan Voron  ",
-            photos:{
-                small:"",
-                large:"https://img.freepik.com/free-vector/skull-moto-helmet_225004-1700.jpg?w=2000",
+            photos: {
+                small: "",
+                large: "https://img.freepik.com/free-vector/skull-moto-helmet_225004-1700.jpg?w=2000",
             },
             status: "Support",
             location: {city: "Brest", country: "Belarus"},
@@ -63,27 +62,31 @@ const Users = () => {
             uniqueUrlName: "",
         },
     ]
-
-if (usersData.length === 0) {
-    axios.get("https://social-network.samuraijs.com/api/1.0/users")
-    .then(response => {dispatch(setUsersAC(response.data.items))})
-    //API Key "e51cb891-4ba3-40e0-8c44-82f372b76693"
-
-}
-debugger
+    let getUsers = () => {
+        if (usersData.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    dispatch(setUsersAC(response.data.items))
+                })
+            //API Key "e51cb891-4ba3-40e0-8c44-82f372b76693"
+        }
+    }
+    useEffect(()=>{getUsers})
     return (
 
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {
-                usersData.map(u=> <div key={u.id}>
+                usersData.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photos.large != null ? u.photos.large : userAvaDefault } alt="Avatar" className={s.userAvatar}/>
+                            <img src={u.photos.large != null ? u.photos.large : userAvaDefault} alt="Avatar"
+                                 className={s.userAvatar}/>
                         </div>
                         <div>
                             {u.followed ?
-                                <button onClick={()=> dispatch(unFollowAC(u.id))}>unFollow</button> :
-                                <button onClick={()=> dispatch(followAC(u.id))}>Follow</button> }
+                                <button onClick={() => dispatch(unFollowAC(u.id))}>unFollow</button> :
+                                <button onClick={() => dispatch(followAC(u.id))}>Follow</button>}
                         </div>
                     </span>
                     <span>
@@ -98,7 +101,7 @@ debugger
                         </span>
                         
                     </span>
-                    
+
                 </div>)
             }
         </div>
